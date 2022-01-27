e8bb5ed11042e8008ef095fbbac88b1cd801d29a
MastDon <sid2@yandex.ru>
2022-01-27 14:17:14 +0200

    Update ssm.py

--- ssm.py
+++ ssm.py
@@ -1,1 +1,35 @@
-print('Hello')
+import boto3
+import time
+import datetime
+
+ec2 = boto3.resource('ec2')
+
+sns = boto3.client('sns', region_name="eu-central-1")
+
+backup_filter = [
+    {
+        'Name': 'tag:Backup',
+        'Values': ['Yes']
+
+    }
+
+]
+
+ami_ids = []
+
+for ins in ec2.instances.filter(Filters=backup_filter):
+    ins.create_image(
+        Name='dev-pro-demo-ami' + str(time.time()),
+        Description='Set your description here ',
+        NoReboot=True
+
+    ),
+    ami_ids.append(ins.instance_id),
+
+    sns.publish(
+        TopicArn='arn:aws:sns:eu-central-1:055524789533:Lambda-Notification',
+        Subject='EC2 backup AMI',
+        Message='Successful create AMI for instances : ' + str(ami_ids),
+
+    )
+
